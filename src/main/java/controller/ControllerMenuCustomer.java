228e050c31f52498ee36f82cdf5b52fdc02e3f93
R0cki99 <Tunning1>
2020-05-24 17:26:35 +0300

    commit-menu

--- src/main/java/controller/ControllerMenuCustomer.java
+++ src/main/java/controller/ControllerMenuCustomer.java
@@ -1,6 +1,7 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -10,8 +11,45 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ControllerMenuCustomer {
-    public void goBackk(ActionEvent event) throws IOException {
+    public void goBackToLogin(ActionEvent event) throws IOException {
         Parent root= FXMLLoader.load(getClass().getClassLoader().getResource("view/sample.fxml"));
+        Scene newwindow=new Scene(root);
+        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(newwindow);
+        window.show();
+    }
+
+    @FXML
+    public void goToSettings(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getClassLoader().getResource("view/Settings.fxml"));
+        Scene newwindow=new Scene(root);
+        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(newwindow);
+        window.show();
+    }
+    @FXML
+    public void goToWorkout(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getClassLoader().getResource("view/Workout.fxml"));
+        Scene newwindow=new Scene(root);
+        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(newwindow);
+        window.show();
+    }
+    @FXML
+    public void goToCalendar(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getClassLoader().getResource("view/LogWorkoutDays.fxml"));
+        Scene newwindow=new Scene(root);
+        Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(newwindow);
+        window.show();
+    }
+    @FXML
+    public void goToPremium(ActionEvent event) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getClassLoader().getResource("view/PremiumSub.fxml"));
         Scene newwindow=new Scene(root);
         Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
 
