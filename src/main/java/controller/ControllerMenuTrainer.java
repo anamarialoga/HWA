6fce545f39e2f547e0d9dc9a4ff497383aab11f7
anamarialoga <anamarialogaa82@gmail.com>
2020-05-25 16:53:45 +0300

    commit-menu-trainer

--- src/main/java/controller/ControllerMenuTrainer.java
+++ src/main/java/controller/ControllerMenuTrainer.java
@@ -1,4 +1,20 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class ControllerMenuTrainer {
+    public void goToSettings2(ActionEvent event) {
+    }
+
+    public void goEditWorkouts(ActionEvent event) {
+    }
+
+    public void goToChat(ActionEvent event) {
+    }
+
+    public void goToCustomersList(ActionEvent event) {
+    }
+
+    public void goBackToLogin(ActionEvent event) {
+    }
 }
